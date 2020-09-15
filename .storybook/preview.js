9d92346010075dc2ac0270decd718a47b2334527
Jack Works <jackworks@protonmail.com>
2020-09-15 16:30:03 +0800

    feat: upgrade to storybook v6

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,36 +1,18 @@
 import '../public/env'
-import { addParameters, addDecorator } from '@storybook/react'
+import * as React from 'react'
 import { withKnobs, radios } from '@storybook/addon-knobs'
 import { MaskbookDarkTheme, MaskbookLightTheme } from '../src/utils/theme'
-import { create } from '@storybook/theming'
+import { I18nextProvider } from 'react-i18next'
+import i18nNextInstance from '../src/utils/i18n-next'
+import { withI18n } from 'storybook-addon-i18n'
+import { ThemeProvider } from '@material-ui/core'
 
-addParameters({
+export const parameters = {
     options: {
-        theme: create({
-            base: 'dark',
-            brandTitle: 'Maskbook',
-            brandUrl: 'https://maskbook.com/',
-            brandImage: 'https://maskbook.com/img/maskbook--logotype-white.png',
-        }),
         isFullscreen: false,
         panelPosition: 'right',
         isToolshown: true,
     },
-})
-// Addons
-addDecorator(withKnobs)
-const themes = { Dark: '0', Light: '1' }
-// Theme for MUI
-addDecorator((storyFn) => (
-    <ThemeProvider theme={[MaskbookDarkTheme, MaskbookLightTheme][radios('Theme', themes, '0')]}>
-        {storyFn()}
-    </ThemeProvider>
-))
-// i18n
-import * as React from 'react'
-import { I18nextProvider } from 'react-i18next'
-import i18nNextInstance from '../src/utils/i18n-next'
-addParameters({
     i18n: {
         provider: function i18nProvider(props) {
             i18nNextInstance.language !== props.locale && i18nNextInstance.changeLanguage(props.locale)
@@ -39,7 +21,15 @@ addParameters({
         supportedLocales: ['en', 'zh'],
         providerLocaleKey: 'locale',
     },
-})
-import { withI18n } from 'storybook-addon-i18n'
-import { ThemeProvider } from '@material-ui/core'
-addDecorator(withI18n)
+}
+const themes = { Dark: '0', Light: '1' }
+export const decorators = [
+    withKnobs,
+    // Theme for MUI
+    (storyFn) => (
+        <ThemeProvider theme={[MaskbookDarkTheme, MaskbookLightTheme][radios('Theme', themes, '0')]}>
+            {storyFn()}
+        </ThemeProvider>
+    ),
+    withI18n,
+]
